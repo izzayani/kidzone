442a6fad3c96136959b3e94be8716dea909f9866
izzayani <47810279+izzayani@users.noreply.github.com>
2019-09-10 15:42:40 +0800

    Add files via upload

---
+++ amalik3 v2/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = '4015';
+$databaseUsername = 'root';
+$databasePassword = '123456';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername,$databasePassword, $databaseName );
+?>
